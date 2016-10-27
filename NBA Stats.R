82e075c2fb9c8e3ed86fe3a115d86144b11dcfe8
kevin-sang <kevin.sang@aet-int.com>
2016-10-27 14:48:15 -0400

    Add files via upload
    Initial commit

---
+++ NBA Stats.R
@@ -0,0 +1,41 @@
+nba_reb_url <- "http://stats.nba.com/stats/leagueleaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2015-16&SeasonType=Regular+Season&StatCategory=REB"
+
+nba_reb_data <- fromJSON(nba_reb_url)
+
+nba_reb_df <- data.frame(nba_reb_data$resultSet$rowSet, stringsAsFactors = FALSE)
+
+typeof(nba_reb_df[[1]])
+
+nba_col_names <- nba_reb_data$resultSet$headers
+
+names(nba_reb_df) <- nba_col_names
+
+
+convert_from_char <- function(x) {
+  for(i in 1:length(nba_col_names)) {
+    if (!is.na(as.numeric(nba_reb_df[ , nba_col_names[i]]))) {
+        nba_reb_df[ , nba_col_names[i]] <- as.numeric(nba_reb_df[, nba_col_names[i]])
+    }
+  }
+}
+
+typeof(nba_reb_data$FT_PCT)
+
+convert_from_factor(nba_col_names)
+
+# plot(as.numeric(as.character(nba_reb_df$REB)), as.numeric(as.character(nba_reb_df$FT_PCT)))
+
+nba_reb_tbl <- as.tbl(nba_reb_df)
+
+adjusted_tbl <- select(arrange(filter(nba_reb_tbl, FT_PCT > 0.6, REB > 8), desc(FT_PCT)), PLAYER, FT_PCT, REB)
+
+arrange(mutate(adjusted_tbl, REB_FT_SCORE = as.numeric(REB)*as.numeric(FT_PCT)), desc(REB_FT_SCORE))
+
+
+# FUTURE DEVELOPMENTS:
+
+# Explore relationship between height and rebounding ability
+
+# Refine Rebound/Freethrow score by determining what percent of rebounds per player leads to positive outcomes
+
+# Figure out how why numeric columns are not being converted to numerics
